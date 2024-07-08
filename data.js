32ac95290b32922e40db763a10f9e48507300174
qinyoulan <qinyoulan@hotmail.com>
2024-07-08 21:24:19 +0800

    landetuijian
    landetuijian

---
+++ data.js
@@ -0,0 +1,25 @@
+// data.js 文件，存储软件列表数据
+export const softwareList = [
+    {
+        name: 'Visual Studio Code',
+        category: '开发工具',
+        icon: 'icons/vscode_icon.png',
+        url: 'https://code.visualstudio.com/',
+        recommendText: '高效编码，必备工具！'
+    },
+    {
+        name: 'Adobe Photoshop',
+        category: '图像编辑',
+        icon: 'icons/photoshop_icon.png',
+        url: 'https://www.adobe.com/products/photoshop.html',
+        recommendText: '设计师的终极工具。'
+    },
+    {
+        name: 'Mozilla Firefox',
+        category: '浏览器',
+        icon: 'icons/firefox_icon.png',
+        url: 'https://www.mozilla.org/en-US/firefox/new/',
+        recommendText: '保护隐私，安全浏览。'
+    },
+    // 可以继续添加更多软件信息
+];
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert('qinyoulan');
+// main.js 文件，用于动态生成磁贴
+import { softwareList } from './data.js'; // 导入软件列表数据
+
+document.addEventListener('DOMContentLoaded', function() {
+    const softwareListElement = document.getElementById('software-list');
+
+    softwareList.forEach(software => {
+        const listItem = document.createElement('li');
+        listItem.className = 'software-tile';
+        const content = `
+            <a href="${software.url}" target="_blank">
+                <img src="${software.icon}" alt="${software.name}">
+                <h3>${software.name}</h3>
+                <p>${software.category}</p>
+                <span class="recommend-text">${software.recommendText}</span>
+            </a>
+        `;
+        listItem.innerHTML = content;
+        softwareListElement.appendChild(listItem);
+    });
+});
