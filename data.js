bf460e40ee2bf5ce0af81ffd4708cc27fa6984b4
qinyoulan <qinyoulan@hotmail.com>
2024-07-08 21:49:57 +0800

    test01

--- data.js
+++ data.js
@@ -15,11 +15,13 @@ export const softwareList = [
         recommendText: '设计师的终极工具。'
     },
     {
-        name: 'Mozilla Firefox',
+        name: 'Via',
         category: '浏览器',
-        icon: 'icons/firefox_icon.png',
-        url: 'https://www.mozilla.org/en-US/firefox/new/',
-        recommendText: '保护隐私，安全浏览。'
+        icon: 'icons/via_icon.png',
+        url: 'https://https://viayoo.com/zh-cn/',
+        recommendText: '探索速度与简约的极限
+
+似乎不凡的速度总会为无所不能的定制让步，似乎简约的界面总会为繁杂的功能牺牲，Via 浏览器在打破陈规旧则，让你看到另种可能。'
     },
     // 可以继续添加更多软件信息
 ];
