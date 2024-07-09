eea24734c9f2039090669a10784e736ba00f685f
qinyoulan <qinyoulan@hotmail.com>
2024-07-09 20:02:36 +0800

    0709

--- data.js
+++ data.js
@@ -8,18 +8,32 @@ export const softwareList = [
         recommendText: '高效编码，必备工具！'
     },
     {
-        name: 'Adobe Photoshop',
-        category: '图像编辑',
-        icon: 'icons/photoshop_icon.svg',
-        url: 'https://www.adobe.com/products/photoshop.html',
-        recommendText: '设计师的终极工具。'
+        name: 'Kimi.ai',
+        category: '网站',
+        icon: 'icons/kimi_icon.png',
+        url: 'https://kimi.moonshot.cn/',
+        recommendText: 'Kimi智能助手'
     },
     {
         name: 'Via',
         category: '浏览器',
         icon: 'icons/via_icon.png',
-        url: 'https://https://viayoo.com/zh-cn/',
+        url: 'https://viayoo.com/zh-cn/',
         recommendText: '探索速度与简约的极限：似乎不凡的速度总会为无所不能的定制让步，似乎简约的界面总会为繁杂的功能牺牲，Via 浏览器在打破陈规旧则，让你看到另种可能。'
+    },
+    {
+        name: 'FreeConvert',
+        category: '网站',
+        icon: 'icons/FreeConvert_icon.png',
+        url: 'https://www.freeconvert.com/',
+        recommendText: '在线格式转换'
+    },
+    {
+        name: '花瓣网',
+        category: '网站',
+        icon: 'icons/huaban.svg',
+        url: 'https://huaban.com/',
+        recommendText: '发现素材，探索灵感'
     },
     // 可以继续添加更多软件信息
 ];
