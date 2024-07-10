dcd29c7bc52f0c192ff7b94aeb222d62e227152e
qinyoulan <qinyoulan@hotmail.com>
2024-07-10 20:43:39 +0800

    1

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-// main.js 文件，用于动态生成磁贴
-import { softwareList } from './data.js'; // 导入软件列表数据
+import { softwareList } from './data.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-    const softwareListElement = document.getElementById('software-list');
+    const recommendations = document.getElementById('recommendations');
 
+    // 创建一个映射，以分类名为键，用于存储每个分类的软件数组
+    const categoryMap = new Map();
+
+    // 填充映射
     softwareList.forEach(software => {
-        const listItem = document.createElement('li');
-        listItem.className = 'software-tile';
-        const content = `
-            <a href="${software.url}" target="_blank">
+        if (!categoryMap.has(software.category)) {
+            categoryMap.set(software.category, []);
+        }
+        categoryMap.get(software.category).push(software);
+    });
+
+    // 根据分类创建HTML结构
+    categoryMap.forEach((softwares, category) => {
+        // 创建分类标题
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.textContent = category;
+        recommendations.appendChild(categoryTitle);
+
+        // 创建列表
+        const categoryList = document.createElement('ul');
+        softwares.forEach(software => {
+            const listItem = document.createElement('li');
+            listItem.className = 'software-tile';
+            listItem.innerHTML = `
                 <img src="${software.icon}" alt="${software.name}">
                 <h3>${software.name}</h3>
-                <p>${software.category}</p>
-                <span class="recommend-text">${software.recommendText}</span>
-            </a>
-        `;
-        listItem.innerHTML = content;
-        softwareListElement.appendChild(listItem);
+                <p>${software.recommendText}</p>
+            `;
+            categoryList.appendChild(listItem);
+        });
+        recommendations.appendChild(categoryList);
     });
 });
