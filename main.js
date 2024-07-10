6325785a395a98a7218af2aeee140eaf964e7b66
qinyoulan <qinyoulan@hotmail.com>
2024-07-10 19:46:50 +0800

    0710

--- main.js
+++ main.js
@@ -5,17 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const softwareListElement = document.getElementById('software-list');
 
     softwareList.forEach(software => {
+        // 创建一个列表项
         const listItem = document.createElement('li');
         listItem.className = 'software-tile';
-        const content = `
-            <a href="${software.url}" target="_blank">
-                <img src="${software.icon}" alt="${software.name}">
-                <h3>${software.name}</h3>
-                <p>${software.category}</p>
-                <span class="recommend-text">${software.recommendText}</span>
-            </a>
+
+        // 创建磁贴内容
+        const tileContent = `
+            <img src="${software.icon}" alt="${software.name}">
+            <h3>${software.name}</h3>
+            <p>${software.category}</p>
+            <a href="${software.url}" target="_blank">了解更多</a>
+            <div class="recommend-text">${software.recommendText}</div>
         `;
-        listItem.innerHTML = content;
+        listItem.innerHTML = tileContent;
         softwareListElement.appendChild(listItem);
     });
 });
