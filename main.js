81241929b2c41221580dec5f4bfd30af5e690c7c
qinyoulan <qinyoulan@hotmail.com>
2024-07-18 18:08:09 +0800

    18

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 // main.js 文件，用于动态生成磁贴
 import { softwareList } from './data.js'; // 导入软件列表数据
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const softwareListElement = document.getElementById('software-list');
 
     softwareList.forEach(software => {
         const listItem = document.createElement('li');
         listItem.className = 'software-tile';
         const content = `
-            <a href="${software.url}" target="_blank">
-                <img src="${software.icon}" alt="${software.name}">
-                <h3>${software.name}</h3>
-                <p>${software.category}</p>
-                <span class="recommend-text">${software.recommendText}</span>
-            </a>
+            <img src="${software.icon}" alt="${software.name}">
+            <h3>${software.name}</h3>
+            <p>${software.category}</p>
+            <p><a href="${software.url}" target="_blank">${software.url}</a></p>
+            <div class="recommend-text">${software.recommendText}</div>
         `;
         listItem.innerHTML = content;
         softwareListElement.appendChild(listItem);
